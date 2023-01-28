f7b878dd7517e4ed42c10576851e75c1c7b3af43
Taylor Fausak <taylor@fausak.me>
2023-01-28 03:21:11 +0000

    Add response logging

--- source/library/Lazuli/Server/Middleware.hs
+++ source/library/Lazuli/Server/Middleware.hs
@@ -3,9 +3,10 @@ module Lazuli.Server.Middleware where
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
 import qualified Data.Vault.Lazy as Vault
-import qualified Data.Word as Word
+import qualified GHC.Clock as Clock
 import qualified Lazuli.Constant.Header as Header
 import qualified Lazuli.Type.RequestId as RequestId
+import qualified Network.HTTP.Types as Http
 import qualified Network.Wai as Wai
 import qualified System.Random as Random
 import qualified Text.Printf as Printf
@@ -15,5 +16,16 @@ middleware :: Vault.Key RequestId.RequestId -> Wai.Middleware
 middleware requestIdKey handle request respond = do
   requestId <- Random.randomIO
   let vault = Vault.insert requestIdKey requestId $ Wai.vault request
+  before <- Clock.getMonotonicTime
   handle request {Wai.vault = vault} $ \response -> do
-    respond $ Wai.mapResponseHeaders ((Header.lazuliRequestId, Text.encodeUtf8 . Text.pack . Printf.printf "%016x" $ Witch.into @Word.Word64 requestId) :) response
+    after <- Clock.getMonotonicTime
+    Printf.printf
+      "%s %s %s%s %d %.3f\n"
+      (Witch.into @String requestId)
+      (Text.decodeUtf8Lenient $ Wai.requestMethod request)
+      (Text.decodeUtf8Lenient $ Wai.rawPathInfo request)
+      (Text.decodeUtf8Lenient $ Wai.rawQueryString request)
+      (Http.statusCode $ Wai.responseStatus response)
+      (after - before)
+    let header = (Header.lazuliRequestId, Text.encodeUtf8 . Text.pack $ Witch.into @String requestId)
+    respond $ Wai.mapResponseHeaders (header :) response
--- source/library/Lazuli/Type/RequestId.hs
+++ source/library/Lazuli/Type/RequestId.hs
@@ -2,11 +2,15 @@ module Lazuli.Type.RequestId where
 
 import qualified Data.Word as Word
 import qualified System.Random as Random
+import qualified Text.Printf as Printf
 import qualified Witch
 
 newtype RequestId
-  = RequestId Word.Word64
+  = RequestId Word.Word32
   deriving (Eq, Show)
-  deriving (Random.Random) via Word.Word64
+  deriving (Random.Random) via Word.Word32
 
-instance Witch.From RequestId Word.Word64
+instance Witch.From RequestId Word.Word32
+
+instance Witch.From RequestId String where
+  from = Printf.printf "%08x" . Witch.into @Word.Word32
